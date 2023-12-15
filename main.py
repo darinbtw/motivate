5eecf296cd9614da236a9cb9deb2f0b7011e8c44
darinbtw <vladik.podyachev@mail.ru>
2023-12-15 21:24:19 +0300

    end code

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
-import phonenumbers
-import random
-import string
+import sqlite3
+from datetime import datetime, timedelta
 
 class RegistrationWindow:
-    def __init__(self, master, app):
+    def __init__(self, master):
         self.master = master
         self.master.title("Registration")
         self.master.geometry("400x300")
-        self.app = app
 
         self.create_widgets()
 
@@ -35,106 +33,60 @@ class RegistrationWindow:
         self.register_button = tk.Button(self.master, text="Register", command=self.register)
         self.register_button.pack(pady=10)
 
-    def validate_phone(self, phone):
-        try:
-            parsed_phone = phonenumbers.parse(phone, "RU")
-            return phonenumbers.is_valid_number(parsed_phone)
-        except phonenumbers.NumberParseException:
-            return False
-
     def register(self):
         username = self.username_entry.get()
         password = self.password_entry.get()
         phone = self.phone_entry.get()
 
-        if not self.validate_phone(phone):
-            messagebox.showwarning("Error", "Invalid phone number format. Please enter a valid phone number.")
-            return
+        # Проверяем, что телефонный номер содержит ровно 10 цифр
+        if len(phone) == 10 and phone.isdigit():
+            # Сохраняем данные в базе данных
+            self.save_user_data(username, password, phone)
 
-        verification_code = ''.join(random.choice(string.digits) for _ in range(6))
-        print(f"Verification Code for {phone}: {verification_code}")
+            # Открываем главное окно приложения
+            self.open_main_app(username)
+        else:
+            messagebox.showinfo("Invalid Input", "Please enter a valid 10-digit phone number.")
 
-        # Здесь можно реализовать отправку кода на телефон (например, через SMS)
+    def save_user_data(self, username, password, phone):
+        # Создаем подключение к базе данных
+        conn = sqlite3.connect("user_data.db")
+        cursor = conn.cursor()
 
-        # Открываем новое окно для ввода кода подтверждения
-        verify_window = tk.Toplevel(self.master)
-        VerifyPhoneWindow(verify_window, self.app, phone)
+        # Создаем таблицу, если она еще не создана
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
+                          (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                           username TEXT,
+                           password TEXT,
+                           phone TEXT)''')
+        conn.commit()
 
+        # Вставляем данные пользователя
+        cursor.execute("INSERT INTO users (username, password, phone) VALUES (?, ?, ?)", (username, password, phone))
+        conn.commit()
+
+        # Закрываем соединение
+        conn.close()
+
+    def open_main_app(self, username):
         # Закрываем окно регистрации
         self.master.destroy()
 
-class VerifyPhoneWindow:
-    def __init__(self, master, app, phone):
-        self.master = master
-        self.master.title("Verify Phone")
-        self.master.geometry("400x300")
-        self.app = app
-        self.phone = phone
+        # Открываем главное окно приложения
+        root = tk.Tk()
+        MainApp(root, username)
+        root.mainloop()
 
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.label = tk.Label(self.master, text=f"Verify Phone for {self.phone}")
-        self.label.pack(pady=10)
-
-        self.verify_code_label = tk.Label(self.master, text="Enter the verification code:")
-        self.verify_code_label.pack()
-
-        self.verify_code_entry = tk.Entry(self.master)
-        self.verify_code_entry.pack()
-
-        self.verify_button = tk.Button(self.master, text="Verify", command=self.verify)
-        self.verify_button.pack(pady=10)
-
-    def verify(self):
-        entered_code = self.verify_code_entry.get()
-        # Здесь можно реализовать проверку кода подтверждения
-        # (например, сравнение с кодом, отправленным пользователю)
-
-        # В данном примере просто выводим сообщение
-        messagebox.showinfo("Verification", "Phone verified successfully!")
-
-        # Закрываем окно подтверждения
-        self.master.destroy()
-
-        # Регистрация пользователя
-        # (здесь можно добавить логику сохранения данных в базу данных)
-
-        # Открываем новое окно после успешной регистрации
-        self.app.open_dashboard()
-
-class DashboardWindow:
+class LoginWindow:
     def __init__(self, master):
         self.master = master
-        self.master.title("Меню мотивации")
+        self.master.title("Login")
         self.master.geometry("400x300")
 
         self.create_widgets()
 
     def create_widgets(self):
-        self.label = tk.Label(self.master, text="Добро пожаловать в меню")
-        self.label.pack()
-
-        self.set_goal_button = tk.Button(self.master, text="Поставить цель", command=self.set_goal)
-        self.set_goal_button.pack(pady=10)
-
-        #self.set_input_goal = tk.Label(self.master, text=)
-
-    def set_goal(self):
-        # Здесь вы можете добавить логику установки цели
-        # В данном примере, просто выводим сообщение
-        messagebox.showinfo("Set Goal", "Goal set successfully!")
-
-class MotivationApp:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Motivation App")
-        self.master.geometry("400x300")
-
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.label = tk.Label(self.master, text="Motivation App")
+        self.label = tk.Label(self.master, text="Login")
         self.label.pack(pady=10)
 
         self.username_label = tk.Label(self.master, text="Username:")
@@ -150,23 +102,143 @@ class MotivationApp:
         self.login_button = tk.Button(self.master, text="Login", command=self.login)
         self.login_button.pack(pady=10)
 
-        self.register_button = tk.Button(self.master, text="Register", command=self.open_registration)
+        self.register_button = tk.Button(self.master, text="Register", command=self.show_registration_window)
         self.register_button.pack(pady=10)
 
     def login(self):
-        # Здесь вы можете добавить логику проверки логина и пароля в базе данных
-        # В данном примере, просто открываем новое окно
-        self.open_dashboard()
+        username = self.username_entry.get()
+        password = self.password_entry.get()
 
-    def open_dashboard(self):
-        self.dashboard_window = tk.Toplevel(self.master)
-        DashboardWindow(self.dashboard_window)
+        # Проверяем, существует ли пользователь с такими данными в базе данных
+        if self.check_user_credentials(username, password):
+            # Закрываем окно входа
+            self.master.destroy()
 
-    def open_registration(self):
-        registration_window = tk.Toplevel(self.master)
-        RegistrationWindow(registration_window, self)
+            # Открываем главное окно приложения
+            root = tk.Tk()
+            MainApp(root, username)
+            root.mainloop()
+        else:
+            messagebox.showinfo("Invalid Credentials", "Incorrect username or password.")
+
+    def check_user_credentials(self, username, password):
+        # Создаем подключение к базе данных
+        conn = sqlite3.connect("user_data.db")
+        cursor = conn.cursor()
+
+        # Выполняем запрос к базе данных
+        cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        user = cursor.fetchone()
+
+        # Закрываем соединение
+        conn.close()
+
+        return user is not None
+
+    def show_registration_window(self):
+        # Закрываем окно входа
+        self.master.destroy()
+
+        # Открываем окно регистрации
+        registration_window = tk.Tk()
+        RegistrationWindow(registration_window)
+
+class MainApp:
+    def __init__(self, master, username):
+        self.master = master
+        self.master.title("Main App")
+        self.master.geometry("400x300")
+        self.username = username
+
+        # Добавляем переменные для цели и времени выполнения
+        self.goal_var = tk.StringVar()
+        self.end_date_var = tk.StringVar()
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label = tk.Label(self.master, text=f"Main App - Welcome, {self.username}!")
+        self.label.pack(pady=10)
+
+        # Добавляем текстовое поле для ввода цели
+        self.goal_label = tk.Label(self.master, text="Set your goal:")
+        self.goal_label.pack()
+        self.goal_entry = tk.Entry(self.master, textvariable=self.goal_var)
+        self.goal_entry.pack()
+
+        # Добавляем текстовое поле для ввода времени выполнения
+        self.end_date_label = tk.Label(self.master, text="Set the end date (format: dd.mm.yyyy):")
+        self.end_date_label.pack()
+        self.end_date_entry = tk.Entry(self.master, textvariable=self.end_date_var)
+        self.end_date_entry.pack()
+
+        # Добавляем кнопку для установки цели и времени выполнения
+        self.set_goal_button = tk.Button(self.master, text="Set Goal", command=self.set_goal)
+        self.set_goal_button.pack(pady=10)
+
+        # Добавляем кнопку для перехода к окну покупки подписки
+        self.purchase_button = tk.Button(self.master, text="Purchase Subscription", command=self.show_purchase_window)
+        self.purchase_button.pack(pady=10)
+
+    def set_goal(self):
+        goal = self.goal_var.get()
+        end_date = self.end_date_var.get()
+
+        # Проверяем, что цель и дата введены
+        if goal and end_date:
+            # Обработка логики установки цели и времени выполнения
+            # Здесь вы можете добавить логику сохранения данных в базе данных или другие действия
+
+            # Пример: выводим сообщение
+            messagebox.showinfo("Goal Set", f"Goal: {goal}\nEnd Date: {end_date}")
+        else:
+            messagebox.showinfo("Invalid Input", "Please enter both goal and end date.")
+
+    def show_purchase_window(self):
+        # Открываем окно покупки подписки
+        purchase_window = tk.Toplevel(self.master)
+        PurchaseSubscriptionWindow(purchase_window)
+
+class PurchaseSubscriptionWindow:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Purchase Subscription")
+        self.master.geometry("400x300")
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label = tk.Label(self.master, text="Purchase Subscription")
+        self.label.pack(pady=10)
+
+        self.card_number_label = tk.Label(self.master, text="Введите вашу карту (16 цифр):")
+        self.card_number_label.pack()
+        self.card_number_entry = tk.Entry(self.master, show="*")
+        self.card_number_entry.pack()
+
+        self.cvv_label = tk.Label(self.master, text="Введите CVV (3 цифр):")
+        self.cvv_label.pack()
+        self.cvv_entry = tk.Entry(self.master, show="*")
+        self.cvv_entry.pack()
+
+        self.purchase_button = tk.Button(self.master, text="Purchase", command=self.purchase_subscription)
+        self.purchase_button.pack(pady=10)
+
+    def purchase_subscription(self):
+        card_number = self.card_number_entry.get()
+        cvv = self.cvv_entry.get()
+
+        # Проверяем, что введены корректные данные
+        if len(card_number) == 16 and card_number.isdigit() and len(cvv) == 3 and cvv.isdigit():
+            # Обработка логики покупки подписки
+            # Здесь вы можете добавить логику сохранения данных в базе данных или другие действия
+
+            # Пример: выводим сообщение
+            messagebox.showinfo("Purchase Successful", "Subscription purchased successfully!")
+        else:
+            messagebox.showinfo("Invalid Input", "Please enter valid card number and CVV.")
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = MotivationApp(root)
+    login_window = LoginWindow(root)
     root.mainloop()
