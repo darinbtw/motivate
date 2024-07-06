7f0d0ab4b74738fa4ffd27a3f4df3e9119a8e8c3
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-07-06 10:06:41 +0300

    added a personal account

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-from PyQt5.QtWidgets import QWidget, QMessageBox, QPushButton, QLabel, QLayout, QMainWindow, QApplication
+from PyQt5.QtWidgets import QWidget, QMessageBox, QPushButton, QLabel, QLayout, QMainWindow, QApplication, QVBoxLayout
 import sys
 
 class Main_Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(500, 300)
         #self.setWindowIcon()
         self.setWindowTitle('Motivatly - Мотивация')
+        self.main_menu()
         
     def main_menu(self):
+        self.setFixedSize(500, 200)
         central_widget = QWidget()
-        self.centralWidget(central_widget)
+        self.setCentralWidget(central_widget)
 
+        self.layout1 = QVBoxLayout()
+
+        self.hello = QLabel('''Приветствую в замечательном приложении, для продуктивных людей!
+В этом приложении вы сможете отмечать свои задачи и составлять к ним дедлай, а если вы не сможете
+их выполнить/отметить, то применится некоторая блокировка, в которой все соц.сети, игры, будут выключатся до срока истечения таймера''')
+        self.layout1.addWidget(self.hello)
+
+        self.button_to_start = QPushButton('Войти в ваш личный кабинет')
+        self.button_to_start.clicked.connect(self.my_door)
+        self.layout1.addWidget(self.button_to_start)
+
+        central_widget.setLayout(self.layout1)
+
+    def my_door(self):
+        central_widged = QWidget()
+        self.setCentralWidget(central_widged)
+        self.setWindowTitle('Ваш личный кабинет')
+
+        self.layout2 = QVBoxLayout()
+
+        self.hello_you = QLabel('Здравствуйте, рады вас видеть!')
+        self.layout2.addWidget(self.hello_you)
+
+        self.layout2.addStretch()
+
+
+        central_widged.setLayout(self.layout2)
 
 
 def start():
