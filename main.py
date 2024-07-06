8ff20921fd86d32b425b5880ca750aaee27ea5aa
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-07-06 10:49:51 +0300

    added edit text from optinon.txt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QWidget, QMessageBox, QPushButton, QLabel, QLayout, QMainWindow, QApplication, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QMessageBox, QPushButton, QLabel, QLayout, QMainWindow, QApplication, QVBoxLayout, QTextEdit
+from PyQt5.QtCore import Qt
 import sys
+import os
 
 class Main_Window(QMainWindow):
     def __init__(self):
@@ -28,6 +30,7 @@ class Main_Window(QMainWindow):
 
     def my_door(self):
         central_widged = QWidget()
+        self.setFixedSize(500,250)
         self.setCentralWidget(central_widged)
         self.setWindowTitle('Ваш личный кабинет')
 
@@ -36,13 +39,34 @@ class Main_Window(QMainWindow):
         self.hello_you = QLabel('Здравствуйте, рады вас видеть!')
         self.layout2.addWidget(self.hello_you)
 
+        self.right_side = QLabel('Впишите задачу, которую вы хотите выполнить')
+        self.right_side.setAlignment(Qt.AlignRight)
+        self.layout2.addWidget(self.right_side)
+
         self.layout2.addStretch()
+
+        self.text1 = QLabel('Ваши задачи:')
+        self.layout2.addWidget(self.text1)
+
+        self.text_edit = QTextEdit()
+        self.text_edit.setFixedSize(200,200)
+        self.text_edit.setReadOnly(True)
+
+        with open('option.txt', 'r', encoding='UTF-8') as file:
+            self.text_edit.setText(file.read())
+        self.layout2.addWidget(self.text_edit)
+
 
 
         central_widged.setLayout(self.layout2)
 
+def check_and_create_file(file_name):
+    if not os.path.exists(file_name):
+        with open(file_name, 'w') as file:
+            file.write('')
 
 def start():
+    check_and_create_file('option.txt')
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
     window = Main_Window()
